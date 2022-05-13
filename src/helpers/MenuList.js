50da084f601b795306eb5fae2e825bd88d4422c7
GosettySidhartha <sidhartha@GosettySidharthas-MacBook-Air.local>
2022-05-13 09:06:36 +0530

    first app

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,47 @@
+//import Pepperoni from "../assets/pepperoni.jpeg";
+import Margherita from "../assets/margherita.jpeg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpeg";
+import Vegan from "../assets/vegan.jpeg";
+import Pineapple from "../assets/pineapple.jpeg";
+import Expensive from "../assets/expensive.jpeg";
+import PizzaLeft from "../assets/pizzaLeft.jpeg";
+import newpaper from "../assets/newpaper.jpeg";
+
+
+export const MenuList = [
+  {
+    name: "Newspaper",
+    image: newpaper,
+    price: 10,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: PizzaLeft,
+    price: 1997.99,
+  },
+];
